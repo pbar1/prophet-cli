cdbb7dd33f65e0151ee29d9cf379d47a20ebea39
Pierce Bartine <piercebartine@gmail.com>
2020-11-20 00:35:38 -0600

    Add readme, refactor flags

---
+++ README.md
@@ -0,0 +1,14 @@
+# `prophet-cli`
+
+```
+Usage: prophet-cli.py [OPTIONS]
+
+  Command line interface for Facebook Prophet forecasting procedure.
+
+Options:
+  --infile TEXT      File to read input from. Stdin if not specified.
+  --outfile TEXT     Save forecast figure to this file.
+  --periods INTEGER  Number of periods (days) ahead to forecast.
+  --demo             Run the Peyton Manning Wikipedia demo forecast.
+  --help             Show this message and exit.
+```
--- prophet-cli.py
+++ prophet-cli.py
@@ -1,44 +1,52 @@
-import click
 import sys
+import click
 import pandas as pd
 from fbprophet import Prophet
 
 
 @click.command()
-@click.argument('filename')
+@click.option('--infile',
+              default='-',
+              help='File to read input from. Stdin if not specified.')
+@click.option('--outfile',
+              default='forecast.png',
+              help='Save forecast figure to this file.')
+@click.option('--periods',
+              default=365,
+              help='Number of periods (days) ahead to forecast.')
 @click.option('--demo',
               is_flag=True,
               default=False,
               help='Run the Peyton Manning Wikipedia demo forecast.')
-@click.option('--output',
-              default='forecast.png',
-              help='Save generated forecast figure to this file.')
-@click.option('--periods',
-              default=365,
-              help='Number of periods (days) ahead to forecast.')
-def main(filename, demo, output, periods):
-    """Command line interface for Facebook Prophet forecasting procedure.
-       Reads from FILENAME if given, or from stdin if not."""
-
+def main(infile, outfile, periods, demo):
+    """Command line interface for Facebook Prophet forecasting procedure."""
     if demo:
-        df = pd.read_csv('./test/example_wp_log_peyton_manning.csv')
-        click.echo(df.head())
+        payton(outfile)
+    else:
+        df = pd.read_csv(sys.stdin if (
+            infile == '' or infile == '-') else infile)
+        df.rename(columns={
+            df.columns[0]: 'ds',
+            df.columns[1]: 'y'
+        },
+                  inplace=True)
         m = Prophet()
         m.fit(df)
-        future = m.make_future_dataframe(periods=365)
-        click.echo(future.tail())
+        future = m.make_future_dataframe(periods=periods)
         forecast = m.predict(future)
-        click.echo(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-        fig1 = m.plot(forecast)
-        fig1.savefig(output)
+        m.plot(forecast).savefig(outfile)
 
-    df = pd.read_csv(sys.stdin if (
-        filename == '' or filename == '-') else filename)
+
+def payton(outfile):
+    df = pd.read_csv('./test/example_wp_log_peyton_manning.csv')
+    click.echo(df.head())
     m = Prophet()
     m.fit(df)
-    future = m.make_future_dataframe(periods=periods)
+    future = m.make_future_dataframe(periods=365)
+    click.echo(future.tail())
     forecast = m.predict(future)
-    m.plot(forecast).savefig(output)
+    click.echo(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+    m.plot(forecast).savefig(outfile)
 
 
 if __name__ == '__main__':
